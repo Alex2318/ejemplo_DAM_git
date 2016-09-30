25d7c6017b32f5b8d77b124272f16aa0a39249f6
USUARIO <USUARIO@192.168.1.167>
2016-09-30 10:36:31 +0200

    ProbandoString

---
+++ ProbandoString/src/ProbandoString.java
@@ -0,0 +1,13 @@
+
+public class ProbandoString {
+
+	public static void main(String[] args) {
+		//Definicion de variables
+		String s = new String( "Álex es un campeón" );
+		String s1 = new String ("Álex continua siendo un campeón");
+		
+		System.out.println(s);
+		System.out.println(s1);
+	}
+
+}
