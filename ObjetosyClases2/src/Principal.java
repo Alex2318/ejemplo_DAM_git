9987b55c7021eb2cb1415d82eea589e503aee68f
USUARIO <USUARIO@192.168.1.167>
2016-09-30 13:38:53 +0200

    Hemos construido una clase en un constructor que nos sirve para nuestro .java principal

---
+++ ObjetosyClases2/src/Principal.java
@@ -0,0 +1,12 @@
+//Esta es la clase principal
+public class Principal {
+
+	//El punto de entrada a nuestra clase principipal
+	public static void main(String[] args) {
+	
+		//TIPO CLASE nombrevar= new TIPO CLASE ();
+		casa c1=new casa(); 
+		casa c2=new casa();
+	}
+
+}
