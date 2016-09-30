11be8b3c47bbb00a4f893e725b75d59b99a0c024
USUARIO <USUARIO@192.168.1.167>
2016-09-30 15:41:02 +0200

    Variables numéricas

---
+++ Variables2/src/Variables2.java
@@ -0,0 +1,25 @@
+
+public class Variables2 {
+
+	public static void main(String[] args) {
+		//Variables
+		//tipo nombreVariable=valor;
+		int numeroEntero=4;
+		int numeroSinDefinir=0;
+		
+	//Numerós decimales 
+		float decimal=12.5f;
+		double decimalMasLargo=12.5d;
+		
+		
+		//Imprimir números enteros
+		System.out.println(numeroEntero);
+		System.out.println(numeroSinDefinir);
+
+		//Imprimir números decimales
+		System.out.println(decimal);
+		System.out.println(decimalMasLargo);
+		
+	}
+
+}
