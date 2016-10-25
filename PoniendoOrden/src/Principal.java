8e7bcb741bc916983556233afa34d1d1dd5724c8
USUARIO <USUARIO@192.168.1.167>
2016-10-25 15:16:28 +0200

    Ejemplo organización en paquetes

---
+++ PoniendoOrden/src/Principal.java
@@ -0,0 +1,13 @@
+import Visual.Ventana1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Ventana1 v1=new Ventana1();
+			v1.setVisible(true);
+			
+	}
+
+}
