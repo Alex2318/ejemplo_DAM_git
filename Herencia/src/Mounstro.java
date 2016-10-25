482309a0d8a32edb20115dde3ed6cc8b002881d3
USUARIO <USUARIO@192.168.1.167>
2016-10-25 10:18:34 +0200

    Ejemplo Herencia

---
+++ Herencia/src/Mounstro.java
@@ -0,0 +1,15 @@
+
+public class Mounstro extends Personaje{
+
+	private String tipoAtaque="";
+	public Mounstro() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getTipoAtaque() {
+		return tipoAtaque;
+	}
+	public void setTipoAtaque(String tipoAtaque) {
+		this.tipoAtaque = tipoAtaque;
+	}
+
+}
---
+++ Herencia/src/Principal.java
@@ -0,0 +1,25 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Generar un nuevo jugador
+		Jugador player1=new Jugador();
+		
+		player1.setPuntos(10);
+		player1.setArmaActual("Espada");
+		
+		//Generar un mounstro
+		Mounstro ogro=new Mounstro();
+		ogro.setTipoAtaque("hachazo");
+		ogro.setVida(5);
+		
+		//Definimos un personaje de forma general
+		Personaje personaje21=new Personaje();
+		personaje21.setNivel(3);
+		personaje21.setVida(100);
+		
+	}
+
+}
